b4a96f6b57688182b2d41951764c63d909ff8b3e
SeungMin Park <94738749+psm4171@users.noreply.github.com>
2024-03-13 17:45:56 +0900

    Update test.sql

--- script/test.sql
+++ script/test.sql
@@ -1,2 +1,14 @@
 
-// test
+# test: topic table 
+
+CREATE TABLE public.topic
+(
+    id serial NOT NULL,
+    title character varying(50) NOT NULL,
+    body text,
+    created timestamp with time zone NOT NULL DEFAULT now(),
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE IF EXISTS public.topic
+    OWNER to test;
