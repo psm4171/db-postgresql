f8dfac2db36a88a15bd87500d2a9dc3450b6c976
SeungMin Park <94738749+psm4171@users.noreply.github.com>
2024-03-17 22:49:20 +0900

    Create sales.sql

---
+++ db/sales_db/sales.sql
@@ -0,0 +1,14 @@
+CREATE TABLE customer(
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+email VARCHAR(60) NOT NULL,
+company VARCHAR(60) NOT NULL, 
+streat VARCHAR(50) NOT NULL, 
+state CHAR(2) NOT NULL, 
+zip SMALLINT NOT NULL,
+phone VARCHAR(20) NOT NULL,
+birth_date DATE NULL,
+sex CHAR(1) NOT NULL, 
+date_entered TIMESTAMP NOT NULL,
+id SERIAL PRIMARY KEY
+);
